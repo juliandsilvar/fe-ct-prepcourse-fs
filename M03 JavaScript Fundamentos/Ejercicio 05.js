9c9373345207670ecaa0787f71a9f009dad385ac
Master <juliandsilvar@gmail.com>
2023-03-24 18:52:06 -0500

    ej 05 m3

--- M03 JavaScript Fundamentos/Ejercicio 05.js
+++ M03 JavaScript Fundamentos/Ejercicio 05.js
@@ -8,46 +8,85 @@ function esPositivo(num) {
    // Si el número es negativo ---> "Es negativo".
    // Si el número es 0, devuelve false.
    // Tu código:
+   console.log("ingrese numero para verificar si es positivo o negativo");
+   var numero;
+   if(numero==0){
+      return false;
+   }
+   else if(numero>0){
+      return "Es positivo";
+   }
+   else return "Es negativo";
 }
 
 function agregarSimboloExclamacion(str) {
    // Agrega un símbolo de exclamación al final del string "str" y retórnalo
    // Ejemplo: "hello world" ---> "hello world!"
    // Tu código:
+   console.log("Ingrese el texto deseado");
+  var str;
+  return str + "!";
 }
 
 function combinarNombres(nombre, apellido) {
    // Retorna "nombre" y "apellido" combinados en un mismo string pero separados por un espacio.
    // Ejemplo: ("Soy", "Henry") ---> "Soy Henry"
    // Tu código:
+   console.log("Ingrese nombre");
+  var nombre;
+  console.log("Ingrese apellido");
+  var apellido;
+  return nombre+" "+apellido;
 }
 
 function obtenerSaludo(nombre) {
    // Toma el string "nombre" y concatena otra string en la cadena para que tome la siguiente forma:
    // Ejemplo: "Martin" ---> "Hola Martin!"
    // Tu código:
+   console.log("Ingrese nombre");
+  var nombre;
+  var saludo="Hola "+nombre+"!";
+  return saludo;
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
    // Retornar el área de un rectángulo teniendo su altura y ancho.
    // Tu código:
+   console.log("Ingrese por favor el alto del rectangulo");
+  var alto;
+  console.log("Ingrese por favor el ancho del rectangulo");
+  var ancho;
+  var area=alto*ancho;
+  return area;
 }
 
 function retornarPerimetro(lado) {
    // La función recibe como argumento la medida de un lado de un cuadrado.
    // Debes retornar su perímetro.
    // Tu código:
+   console.log("Ingrese el tamaño de uno de sus lados");
+  var lado;
+  return lado*4;
 }
 
 function areaDelTriangulo(base, altura) {
    // Calcula el área de un triángulo y retorna el resultado.
    // Tu código:
+   console.log("Ingrese por favor la base del triangulo");
+  var base;
+  console.log("Ingrese por favor la altura del triangulo");
+  var altura;
+  var area=(base*altura)/2;
+  return area;
 }
 
 function deEuroAdolar(euro) {
    // Supongamos que 1 euro equivale a 1.20 dólares.
    // Debes calcular el valor recibido como argumento pasándolo a dolares.
    // Tu código:
+   console.log("ingrese la cantidad de euros a convertir");
+  var euro;
+  return euro*1.20;
 }
 
 function esVocal(letra) {
@@ -55,6 +94,19 @@ function esVocal(letra) {
    // Si el usuario ingresa un string de más de un caracter debes retornar el mensaje: "Dato incorrecto".
    // Si no es vocal, tambien debe retornar "Dato incorrecto".
    // Tu código:
+   console.log("Ingrese una letra para determinar si es vocal");
+  var letra;
+  if(letra==="a"){
+    return "Es vocal";
+  }if(letra==="e"){
+    return "Es vocal";
+  }if(letra==="i"){
+    return "Es vocal";
+  }if(letra==="o"){
+    return "Es vocal";
+  }if(letra==="u"){
+    return "Es vocal";
+  }else return "Dato incorrecto";
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
