1ecc6282046fd23c2c76d59393cb2e917a3adbe2
Master <juliandsilvar@gmail.com>
2023-03-24 19:54:14 -0500

    m4

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -5,6 +5,10 @@ function obtenerMayor(x, y) {
    // Retornar el número más grande.
    // Si son iguales, retornar cualquiera de los dos.
    // Tu código:
+   var x, y;
+  if(x>y){
+    return x;
+  }return y;
 }
 
 function mayoriaDeEdad(edad) {
@@ -12,6 +16,12 @@ function mayoriaDeEdad(edad) {
    // Si tiene 18 años ó más debe retornar el string: "Allowed".
    // Caso contrario: "Not allowed".
    // Tu código:
+   var edad;
+  if(edad>=18){
+    return "Allowed";
+  }else {
+    return "Not allowed";
+  }
 }
 
 function conection(status) {
@@ -21,6 +31,12 @@ function conection(status) {
    // De lo contrario, presumimos que el usuario está "Offline".
    // Retornar el estado de conexión del usuario.
    // Tu código:
+   var status;
+  if(status===1){
+    return "Online";
+  }if(status===2){
+    return "Away";
+  }else return "Offline";
 }
 
 function saludo(idioma) {
@@ -30,6 +46,14 @@ function saludo(idioma) {
    // Si "idioma" es "ingles", devuelve "Hello!".
    // Si "idioma" no es ninguno de los anteriores o es `undefined` devuelve "Hola!".
    // Tu código:
+   var idioma;
+   if(idioma==="aleman"){
+      return "Guten Tag!";
+   }if(idioma==="mandarin"){
+      return "Ni Hao!";
+   }if(idioma==="ingles"){
+      return "Hello!"
+   }else return "Hola!";
 }
 
 function colors(color) {
@@ -41,6 +65,16 @@ function colors(color) {
    // Si no es ninguno de esos colores           --> "Color not found".
    // IMPORTANTE: utilizar el statement SWITCH.
    // Tu código:
+   var color;
+   if(color==="blue"){
+      return "This is blue";
+   }if(color==="red"){
+      return "This is red";
+   }if(color==="green"){
+      return "This is green";
+   }if(color==="orange"){
+      return "This is orange";
+   }else return "Color not found";
 }
 
 function esDiezOCinco(num) {
