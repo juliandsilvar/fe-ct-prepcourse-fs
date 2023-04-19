515059d16f2a5c6fc789a87376b07e15e339a4e0
juliandsilvar <juliandsilvar@gmail.com>
2023-04-19 09:53:25 -0500

    07 terminado faltante practica

--- M07 JavaScript Clases/Ejercicio 01.js
+++ M07 JavaScript Clases/Ejercicio 01.js
@@ -36,6 +36,9 @@ function agregarStringInvertida() {
    // Agrega un método al prototipo de "String" que devuelva la misma cadena de caracteres, pero invertida.
    // El método debe llamarse "reverse".
    // [PISTA]: necesitarás utilizar el objeto "this".
+   String.prototype.reverse=function(){
+      return this.split('').reverse().join('');
+   }
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
